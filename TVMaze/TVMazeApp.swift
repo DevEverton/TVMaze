a939a0aa6ca0c592d7c434b292ceee830d2fd132
Everton Carneiro <everton.iosdev@gmail.com>
2025-04-23 16:23:58 -0300

    Initial Commit

---
+++ TVMaze/TVMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeApp.swift
+//  TVMaze
+//
+//  Created by Everton Carneiro on 23/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TVMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
