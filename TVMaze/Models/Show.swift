42859db5796db3450fd4880509c1b0e7dc87ff2d
Everton Carneiro <everton.iosdev@gmail.com>
2025-04-24 17:45:38 -0300

    Fix decoding problem when no summary is available

--- TVMaze/Models/Show.swift
+++ TVMaze/Models/Show.swift
@@ -5,4 +5,18 @@ struct Show: Identifiable, Decodable {
     let name: String
     let image: ImageResponse?
     let summary: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, image, summary
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        image = try container.decodeIfPresent(ImageResponse.self, forKey: .image)
+        
+        let rawSummary = try container.decodeIfPresent(String.self, forKey: .summary)
+        summary = rawSummary?.strippedHTML ?? "No description available"
+    }
 } 
